e1f829039ebcc0bebf0847be54a7c13da535e2ad
Adnane MBA15 <astit103@gmail.com>
2024-05-26 19:20:08 +0100

    First run

--- ScrapyURLs/spiders/daod.py
+++ ScrapyURLs/spiders/daod.py
@@ -4,7 +4,8 @@ import scrapy
 class DaodSpider(scrapy.Spider):
     name = "daod"
     allowed_domains = ["www.canada.ca"]
-    start_urls = ["https://www.canada.ca/en/department-national-defence/corporate/policies-standards/defence-administrative-orders-directives/1000-series.html"]
+    start_urls = ["https://www.canada.ca/en/department-national-defence/corporate/policies-standards/defence-administrative-orders-directives/1000-series/1000/1000-0-foundation-framework-defence-administrative-orders-directives.html"]
 
     def parse(self, response):
+        print('Hello World !!! of Scrapy !!')
         pass
