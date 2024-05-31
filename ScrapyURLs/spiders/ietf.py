edaa7d1db64fffd5a680fc1a53fb2c987d4f68ba
Adnane MBA15 <astit77@gmail.com>
2024-05-31 21:00:51 +0100

    ietf added

---
+++ ScrapyURLs/spiders/ietf.py
@@ -0,0 +1,14 @@
+import scrapy
+
+
+class IetfSpider(scrapy.Spider):
+    name = "ietf"
+    allowed_domains = ["canada.ca"]
+    start_urls = ["https://www.canada.ca/en/department-national-defence/corporate/policies-standards/defence-administrative-orders-directives.html"]
+
+    def parse(self, response):
+        # rfc = response.xpath('//span[@class="rfc-no"]//get()')
+        # title = response.css("span.title::text").get()
+        description = response.xpath('//div[@class="row wb-eqht-grd"]//h3//a/text()').get()
+
+        yield {'description': description}
