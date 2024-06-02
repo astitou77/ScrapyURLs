cac64897074037c0132ca11cd13e5eff29438b27
Adnane MBA15 <astit77@gmail.com>
2024-06-03 00:12:58 +0100

    localhost tests

--- ScrapyURLs/spiders/myspider.py
+++ ScrapyURLs/spiders/myspider.py
@@ -5,16 +5,16 @@ from scrapy.spiders import CrawlSpider, Rule
 
 class MyspiderSpider(CrawlSpider):
     name = "myspider"
-    allowed_domains = ["www.canada.ca"]
-    start_urls = ["https://www.canada.ca/en/department-national-defence/corporate/policies-standards/defence-administrative-orders-directives.html"]
+    # allowed_domains = ["localhost:700"]
+    start_urls = ["http://localhost:700/index.html"]
     custom_settings = {
-        'DEPTH_LIMIT': 1,  # Limit the depth to 1
+        'DEPTH_LIMIT': 2,  # Limit the depth to 1
     }
 
     rules = (
         Rule(
             # allow=('\/.*',), 
-            LinkExtractor(restrict_css='div.mwsbodytext'), 
+            LinkExtractor(restrict_css='div.mwsbodytext ul li'), 
             callback='parse_item', 
             follow=True),
     )
@@ -24,23 +24,13 @@ class MyspiderSpider(CrawlSpider):
         links = response.css('a::attr(href)').extract()
         for link in links:
             full_url = response.urljoin(link)
+            status = response.status
             # Check if the link is external (outside 'mysite.com')
-            if not link.startswith('https://www.canada.ca'):
+            if not link.startswith('http://localhost:700'):
                 # Yield a dictionary with the external URL and status code
                 yield {
                     'url': full_url,
-                    'status': None  # Set status to None for external links
+                    'status': status  # Set status to None for external links
                 }
-            else:
-                # Follow internal links within the specified path
-                yield scrapy.Request(link, callback=self.parse_item)
 
-        # Extract the URL and status code from the response
-        url = response.url
-        status = response.status
-        # Yield a dictionary with URL and status code
-        yield {
-            'url': url,
-            'status': status
-        }
 
