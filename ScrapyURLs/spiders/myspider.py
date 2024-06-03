79e9bb77ce61be0d768846f3615a16775c215638
Adnane MBA15 <astit77@gmail.com>
2024-06-03 19:02:39 +0100

    learning more

--- ScrapyURLs/spiders/myspider.py
+++ ScrapyURLs/spiders/myspider.py
@@ -1,3 +1,4 @@
+from typing import Iterable
 import scrapy
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
@@ -8,8 +9,16 @@ class MyspiderSpider(CrawlSpider):
     # allowed_domains = ["localhost:700"]
     start_urls = ["http://localhost:700/index.html"]
     custom_settings = {
-        'DEPTH_LIMIT': 4,  # Limit the depth to 1
+        'DEPTH_LIMIT': 1,  # Limit the depth to 1
     }
+
+    # DEFAULT SCRAPY START FUNCTION - Override
+    # iterates over 'start_urls' to generate HTTP 'Request' objects
+    def start_requests(self):
+        for url in self.start_urls:
+            print('start_requests', 'zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz')
+            print(url)
+            yield scrapy.Request(url, callback=self.parse_link)
 
     # Dictionary to hold the parent-child relationships
     results = {}
@@ -17,28 +26,44 @@ class MyspiderSpider(CrawlSpider):
     rules = (
         Rule(
             # allow=('\/.*',), 
-            LinkExtractor(restrict_css='div.mwsbodytext ul li'), # Extract links within specified CSS selector
-            callback='parse_item', # Process each link with this method
-            follow=True
+            LinkExtractor(restrict_css='div.mwsbodytext ul li a'), # Extract links within specified CSS selector
+            callback='parse_link', # Process each link with this method
+            follow=False,
+            process_request='process_request'  # Modify each (url extracted) request before sending (aka Crawling them)
         ),
     )
 
     def process_request(self, request, response):
-        request.meta['errback'] = self.handle_error
+        print('\nprocess_request', 'bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb')
+        print(request.url, '\n', request.meta)
+        request.meta['errback'] = self.handle_error   # what is meta 'errback' ? default recognized by scrapy ?
+        print(request.url, '\n', request.meta, 'zzzzz\n')
+        return request
+
+    def handle_error(self, failure):
         pass
 
-    def parse_item(self, response):
+    def parse_link(self, response):
+        parent_url = response.meta.get('parent_url', 'root')
+        current_url = response.url
+
+        print("\nparse_link aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+        print(parent_url, current_url)
+        # response.meta
+        # {'rule': 0, 'link_text': 'Homepage, Link 3', 'depth': 1, 'download_timeout': 180.0, 'download_slot': 'localhost', 'download_latency': 0.00288}
+        print(response.meta.get('zozo', 'link_text'),  'aaaa')
+        print('\n\tHTTP: ', response.status, '\tURL: ', response.url, '\n')
+
         # Extract all links from the response
-        links = response.css('a::attr(href)').extract()
+        links = response.css('a::attr(href)').getall()    # extract() vs. getall() ??
         for link in links:
             full_url = response.urljoin(link)
-            status = response.status
             # Check if the link is external (outside 'mysite.com')
             if not link.startswith('http://localhost:700'):
                 # Yield a dictionary with the external URL and status code
                 yield {
-                    'url': full_url,
-                    'status': status  # Set status to None for external links
+                    'urls_inside': full_url
                 }
+                # ask chatGPT to save 'referer' and parent-child ; Crawled (for URLs) vs. Scraped (data requested)
 
 
