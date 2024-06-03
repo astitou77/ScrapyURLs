f5c5e78910c47dd3d5fb024ebaf754bfbb12912d
Adnane MBA15 <astit77@gmail.com>
2024-06-03 23:53:25 +0100

    added relevant scrapy comments

--- ScrapyURLs/spiders/myspider.py
+++ ScrapyURLs/spiders/myspider.py
@@ -3,35 +3,95 @@ import scrapy
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
 
-
 class MyspiderSpider(CrawlSpider):
-    name = "myspider"
-    # allowed_domains = ["localhost:700"]
-    start_urls = ["http://localhost:700/index.html"]
-    custom_settings = {
-        'DEPTH_LIMIT': 1,  # Limit the depth to 1
-    }
-
-    # DEFAULT SCRAPY START FUNCTION - Override
-    # iterates over 'start_urls' to generate HTTP 'Request' objects
-    def start_requests(self):
-        for url in self.start_urls:
-            print('start_requests', 'zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz')
-            print(url)
-            yield scrapy.Request(url, callback=self.parse_link)
-
     # Dictionary to hold the parent-child relationships
     results = {}
 
+    # PREDEFINED ATTRIBUTE - for 'CrawlSpider' - not 'scrapy.Spider'
+    # 'rule' : Defines a set of rules to follow links and extract data. 
+    #          Each rule is an instance of 'Rule', which defines how to follow links and what callbacks to use.
+    # 
+    #   'LinkExtractor' : Used to specify which links to extract based on URL patterns, domains, XPath, or CSS selectors.
+    #           'allow'           : A regular expression (or list of regexes) that the URLs must match to be extracted.
+    #           'deny'            : A regular expression (or list of regexes) that the URLs must not match to be extracted.
+    #           'allow_domains'   : A list of domains that the URLs must belong to be extracted.
+    #           'deny_domains'    : A list of domains that the URLs must not belong to be extracted.
+    #           'restrict_xpaths' : An XPath (or list of XPaths) to restrict the part of the page where the links should be extracted from.
+    #           'restrict_css'    : A CSS selector (or list of selectors) to restrict the part of the page where the links should be extracted from.
+    #   'callback'      : Method to be called with the response of each link extracted.
+    #   'follow'        : Boolean indicating whether to continue following links from the pages downloaded using this rule.
+    # --------------------------------------------------------------------------------------------------
     rules = (
         Rule(
             # allow=('\/.*',), 
             LinkExtractor(restrict_css='div.mwsbodytext ul li a'), # Extract links within specified CSS selector
             callback='parse_link', # Process each link with this method
+            # errback='parse_err_link',
             follow=False,
             process_request='process_request'  # Modify each (url extracted) request before sending (aka Crawling them)
         ),
     )
+
+    # PREDEFINED ATTRIBUTE
+    # 'name' : identify the spider when running it from the command line
+    # --------------------------------------------------------------------------------------------------
+    name = "myspider"
+
+    # PREDEFINED ATTRIBUTE
+    # 'start_urls' : A list of URLs to start crawling from. 
+    #                If start_requests is not defined, Scrapy generates requests for these URLs.
+    # --------------------------------------------------------------------------------------------------
+    start_urls = ["http://localhost:700/index.html"]
+
+    # PREDEFINED ATTRIBUTE
+    # 'allowed_domains' : A list of domains that the spider is allowed to crawl (download HTML content). 
+    #                     Requests to URLs outside these domains will be ignored.
+    # --------------------------------------------------------------------------------------------------
+    # allowed_domains = ["localhost:700"]
+
+    # PREDEFINED ATTRIBUTE
+    # 'custom_settings' : A dictionary of settings specific to this spider. 
+    #                    These settings override the project-wide settings.
+    # --------------------------------------------------------------------------------------------------
+    custom_settings = {
+        'DEPTH_LIMIT': 1,  # Limit the depth to 1
+        # 'USER_AGENT': 'my-custom-user-agent',
+    }
+
+
+    # PREDEFINED METHOD
+    # 'start_requests' : Generates the initial requests to begin the crawling process. 
+    #                    If not defined, Scrapy uses the start_urls attribute to create these requests.
+    # --------------------------------------------------------------------------------------------------
+    def start_requests(self):
+        for url in self.start_urls:
+            print('start_requests', 'zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz')
+            print(url)
+
+            # PREDEFINED
+            # 'logger' : A Python logger instance for logging messages. 
+            #            This is more flexible and recommended over the log method.
+            # ------------------------------------------------------------------------------------------
+            # self.logger.info('This is an info message')
+
+            # PREDEFINED 'REQUEST' parameters - Crawls each start_urls, and sends HTML to be parsed.
+            # 'callback' :  Specifies the method to be called with the response of the request.
+            # 'errback'  :  Specifies the method to be called if an error occurs during the request.
+            # 'meta'     :  A dictionary of metadata to be passed along with the request. 
+            #               This can include custom data and control information.
+            #               Ex.: yield scrapy.Request(url, callback=self.parse, errback=self.handle_error, meta={'depth': 1})
+            # 'headers'  :  Allows you to set custom headers for the request.
+            #               Ex.: yield scrapy.Request(url, headers={'User-Agent': 'my-custom-user-agent'})
+            # 'cookies'  :  Allows you to send cookies with the request.
+            #               Ex.: yield scrapy.Request(url, cookies={'sessionid': '12345'})
+            #
+            # 'handle_httpstatus_list' : 
+            #               Allows handling specific HTTP status codes as part of normal 
+            #               responses rather than errors. 
+            #               Ex.: yield scrapy.Request(url, callback=self.parse_link, meta={'handle_httpstatus_list': [404, 500]}
+            # ------------------------------------------------------------------------------------------
+            yield scrapy.Request(url, callback=self.parse_link)
+
 
     def process_request(self, request, response):
         print('\nprocess_request', 'bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb')
@@ -43,6 +103,11 @@ class MyspiderSpider(CrawlSpider):
     def handle_error(self, failure):
         pass
 
+    # PREDEFINED METHOD
+    # parse: The default callback method for requests. If not otherwise specified, 
+    #        Scrapy will use this method to handle responses. 
+    #        Typically used to parse the initial response and extract data or additional URLs.
+    # --------------------------------------------------------------------------------------------------
     def parse_link(self, response):
         parent_url = response.meta.get('parent_url', 'root')
         current_url = response.url
@@ -66,4 +131,20 @@ class MyspiderSpider(CrawlSpider):
                 }
                 # ask chatGPT to save 'referer' and parent-child ; Crawled (for URLs) vs. Scraped (data requested)
 
+    # PREDEFINED METHOD
+    # 'errback' : Error handling method for requests. 
+    #             If an error occurs during the processing of a request, 
+    #             the errback method specified in the request's meta data will be called.
+    # --------------------------------------------------------------------------------------------------
+    # def errback(self, failure):
+    #     pass
+
+    # PREDEFINED METHOD
+    # 'closed' : Called when the spider is closed. 
+    #            This can be used to perform any cleanup or final processing. 
+    #            The reason parameter indicates why the spider was closed.
+    # --------------------------------------------------------------------------------------------------
+    # def closed(self, reason):
+    #     # Perform any cleanup or final processing here
+    #     self.log(f"Spider closed because: {reason}")
 
